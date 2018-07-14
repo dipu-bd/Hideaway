cbf4efcc560c7415b42fda3a67909c0d232436ab
dipu-bd <dipu.sudipta@gmail.com>
2018-07-14 13:12:23 +0600

    Create files with checkFolder

--- src/main/java/org/dpulab/hideaway/utils/Storage.java
+++ src/main/java/org/dpulab/hideaway/utils/Storage.java
@@ -5,6 +5,10 @@
  */
 package org.dpulab.hideaway.utils;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystemException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import org.dpulab.hideaway.models.EncryptedFile;
@@ -17,25 +21,48 @@ public class Storage {
     
     private static final HashMap<String, Storage> STORAGE = new HashMap<>();
     
-    public static Storage getStorage(String folder) {
+    public static Storage getStorage() throws FileSystemException, SecurityException {
+        String folder = Settings.getDefault().get("WORK_DIRECTORY");
         if (!Storage.STORAGE.containsKey(folder)) {
-            Storage.STORAGE.put(folder, new Storage(folder));
+            // validate the work folder
+            File workDir = new File(folder);
+            if (!workDir.exists()) {
+                if (!workDir.mkdirs()) {
+                    throw new FileSystemException("Failed to create work directory");
+                }
+                if (!workDir.canWrite()) {
+                    throw new FileSystemException("The work directory do not have write permission");
+                }
+            }
+            // attach a new storage class with the folder
+            Storage.STORAGE.put(folder, new Storage(workDir));
         }
         return Storage.STORAGE.get(folder);
     }
     
-    private final String workPath;
+    private final Path workDir;
     private final ArrayList<EncryptedFile> files;
     private final HashMap<String, String> publicKeys;
     
-    private Storage(String folder) {
-        this.workPath = folder;
+    private Storage(File workDir) {
+        this.workDir = workDir.toPath();
         this.files = new ArrayList<>();
         this.publicKeys = new HashMap<>();
     }
     
-    public void checkFolder() {
+    public void checkFolder() throws IOException {
+        String password = Settings.getDefault().getSession("PASSWORD");
+        String passwordHash = CryptoService.getDefault().getHash(password);
         
+        File data = workDir.resolve("data").toFile();
+        File keys = workDir.resolve("keys").toFile();
+        File index = workDir.resolve(passwordHash + ".index").toFile();
+        
+        if (!data.exists()) data.mkdir();
+        if (!keys.exists()) data.mkdir();
+        if (!index.exists()) {
+            index.createNewFile();
+        }
     }
     
     
