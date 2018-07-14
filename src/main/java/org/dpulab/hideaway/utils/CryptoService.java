fab68cbd3ed7fa20582e86ad8ac1bfc2483d6b9b
dipu-bd <dipu.sudipta@gmail.com>
2018-07-14 21:29:25 +0600

    Update getKeyBlock. Remove encrypt/decrypt methods

--- src/main/java/org/dpulab/hideaway/utils/CryptoService.java
+++ src/main/java/org/dpulab/hideaway/utils/CryptoService.java
@@ -5,22 +5,14 @@
  */
 package org.dpulab.hideaway.utils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.security.GeneralSecurityException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  *
@@ -56,92 +48,23 @@ public class CryptoService {
         return hash;
     }
     
-    protected String getModifierPassword(String password) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(password);
-        sb.reverse();
-        sb.append(password.substring(password.length() / 2));
-        sb.reverse();
-        sb.append(password.substring(0, password.length() / 2));
-        sb.reverse();
-        return sb.toString();
-    }
-    
-    SecretKeySpec generateKeySpec(String password, String algorithm) throws UnsupportedEncodingException {
-        byte[] key = password.getBytes("UTF-8");
-        key = Arrays.copyOf(key, 16); // use only first 128 bit
-        SecretKeySpec spec = new SecretKeySpec(key, algorithm);
-        return spec;
-    }
-    
-    IvParameterSpec generateIV() {
-        return new IvParameterSpec("1123581321345589".getBytes());
-    }
-    
     /**
-     * Encrypt a plain text using AES encryption.
-     * @param plainText The text to encrypt
-     * @param password The password to use
-     * @return The encrypted text
-     * @throws GeneralSecurityException in case of failure in encryption.
+     * Generate password key block of given size.
+     * @param password The password to use (Should be minimum of 8 bytes).
+     * @param blockSize The block size (Supported values: 16, 24, 32).
+     * @return The password block of fixed size.
      * @throws UnsupportedEncodingException 
      */
-    public byte[] encryptAES(final byte[] plainText, String password) throws GeneralSecurityException, UnsupportedEncodingException {
-        // Generate AES key spec
-        password = this.getModifierPassword(password);
-        SecretKeySpec key = generateKeySpec(password, "AES");
-        
-        // Decrypt using AES
-        IvParameterSpec iv = this.generateIV();
-        Cipher decryptor = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        decryptor.init(Cipher.ENCRYPT_MODE, key, iv);
-        return decryptor.doFinal(plainText);
+    public static byte[] getKeyBlock(String password, int blockSize) throws UnsupportedEncodingException {        
+        int sign = 1;
+        while(password.length() < blockSize) {
+            int rotc = sign * password.length() / 4;
+            String temp = StringUtils.rotate(password, rotc);
+            password = StringUtils.reverse(temp.concat(password));
+            sign *= -1;
+        }
+        byte[] bytes = password.getBytes("UTF-8");
+        return Arrays.copyOf(bytes, blockSize);
     }
     
-    /**
-     * Encrypt a plain text using AES and store it into a file.
-     * @param plainText The text to encrypt.
-     * @param outputFile The output file.
-     * @param password The password to use.
-     * @throws java.security.GeneralSecurityException in case of failure in encryption.
-     * @throws java.io.IOException in case of an I/O error.
-     */
-    public void saveEncrypted(final byte[] plainText, final File outputFile, String password) throws GeneralSecurityException, IOException {
-        byte[] cipherText = this.encryptAES(plainText, password);
-        FileUtils.writeByteArrayToFile(outputFile, cipherText);
-    }
-    
-    /***
-     * Decrypt a cipher text encrypted with AES.
-     * @param cipherText The bytes to decrypt.
-     * @param password The password to use.
-     * @return The decrypted plain text.
-     * @throws GeneralSecurityException in case of failure in decryption.
-     * @throws java.io.UnsupportedEncodingException in case UTF-8 encoding scheme not found.
-     */
-    public byte[] decryptAES(final byte[] cipherText, String password) throws GeneralSecurityException, UnsupportedEncodingException {
-        // Generate AES key spec
-        password = this.getModifierPassword(password);
-        SecretKeySpec key = generateKeySpec(password, "AES");
-        
-        // Decrypt using AES
-        IvParameterSpec iv = this.generateIV();
-        Cipher decryptor = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        decryptor.init(Cipher.DECRYPT_MODE, key, iv);
-        return decryptor.doFinal(cipherText);
-    }
-    
-    /**
-     * Decrypt a file encrypted with AES and returns the plain text content.
-     * @param inputFile The file to decrypt.
-     * @param password The password to use.
-     * @return The decrypted plain text.
-     * @throws GeneralSecurityException in case of failure in decryption.
-     * @throws IOException in case of an I/O error.
-     */
-    public byte[]  loadDecrypted(final File inputFile, String password) throws GeneralSecurityException, IOException {
-        byte[] cipherText = FileUtils.readFileToByteArray(inputFile);
-        return this.decryptAES(cipherText, password);
-    }
-
 }
